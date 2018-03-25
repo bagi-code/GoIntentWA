344b8452831b136566b9e1021540b0a73b3e8dc7
Bagicode <bagicode@gmail.com>
2018-03-26 03:55:38 +0700

    Up pilih stringnya aja

--- app/src/main/java/com/bagicode/gointentwa/MainActivity.java
+++ app/src/main/java/com/bagicode/gointentwa/MainActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+/*
+Create by bagicode
+
+Read More : https://faq.whatsapp.com/id/android/26000030/
+*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,8 +22,13 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btnGOWA).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String pakePesan = "https://api.whatsapp.com/send?phone=15551234567&text=Saya%20tertarik%20untuk%20membeli%20mobil%20Anda";
+                String pakePesanAja = "https://api.whatsapp.com/send?text=Saya%20memiliki%20pertanyaan%20mengenai%20apartment%20yang%20disewakan";
+                String pakeNoAja = "https://api.whatsapp.com/send?phone=628561333111";
+
                 Intent i = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://api.whatsapp.com/send?phone=628561333111"));
+                        Uri.parse(pakeNoAja));
                 startActivity(i);
             }
         });
