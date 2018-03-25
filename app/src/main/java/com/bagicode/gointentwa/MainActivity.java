72bd5a592b247a5ef3e7e14aedeb16ee988437da
Bagicode <bagicode@gmail.com>
2018-03-26 03:53:13 +0700

    Initial commit

---
+++ app/src/main/java/com/bagicode/gointentwa/MainActivity.java
@@ -0,0 +1,25 @@
+package com.bagicode.gointentwa;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnGOWA).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://api.whatsapp.com/send?phone=628561333111"));
+                startActivity(i);
+            }
+        });
+    }
+}
